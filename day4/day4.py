4b53a9cebaa77f8084402ff642c6855446220413
corneheijnen <corneheijnen@hellofresh.nl>
2021-12-07 09:59:18 +0100

    dictionary of tables

--- day4/day4.py
+++ day4/day4.py
@@ -21,3 +21,23 @@ with open('day4/input.txt') as file:
             cards_counter += 1
             row = 0
         row += 1
+
+def get_outcome(number_sequence, cards_dict):
+    for number in number_sequence:
+        print(number)
+        print(f"Checking {number} in all bingo cards")
+
+        for key, values in cards_dict.items():
+            cards_dict[key] = values.replace(str(number), -1)
+            if (cards_dict[key].sum() == - 5).any() or (cards_dict[key].sum(axis=1) == - 5).any():
+                print(number)
+                print(cards_dict[key])
+                return cards_dict[key], number
+
+
+frame, number = get_outcome(number_sequence, cards_dict)
+sum = frame.replace(-1, 0)
+for column in sum.columns:
+    sum[column] = sum[column].astype(int)
+
+answer = sum.sum().sum() * int(number)
